4f6e64087b7d7fa1d98c2375e846642fc229ed7b
Ferwin Lopez <ferwinlopezt@gmail.com>
2024-10-05 00:20:59 +0530

    feat : login page done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${spice.className}  antialiased`}
+        className={` antialiased`}
       >
         {children}
       </body>
